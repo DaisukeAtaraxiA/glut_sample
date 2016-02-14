9a8534b7ce55fc4bd07ccc61152e5835b57ad1cf
Daisuke AtaraxiA <d.morikawa.b@gmail.com>
2016-02-14 20:49:29 +0900

    Implement the view rotation.

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,18 @@
 
 namespace {
 
+bool mouse_l = false;
+int mouse_pos[2]{};
+double angle[2]{};
+
 void display() {
   glClearColor(0, 0, 0, 0.0);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  glRotated(angle[0], 1.0, 0.0, 0.0);
+  glRotated(angle[1], 0.0, 1.0, 0.0);
 
   glColor3f(0.5, 0.1, 0.2);
   glBegin(GL_POLYGON);
@@ -34,6 +43,44 @@ void keyboard(unsigned char key, int x, int y) {
   }
 }
 
+void mouse(int button, int state, int x, int y) {
+  switch (button) {
+    case GLUT_LEFT_BUTTON:
+      if (state == GLUT_DOWN) {
+				mouse_pos[0] = x;
+				mouse_pos[1] = y;
+				mouse_l = true;
+      }
+      if (state == GLUT_UP) {
+				mouse_l = false;
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+void motion(int x, int y) {
+  if (mouse_l) {
+    int viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    int width = viewport[2];
+    int height = viewport[3];
+    double theta[2]{};
+    if (height > 0) {
+      theta[0] = static_cast<double>(y - mouse_pos[1]) * 180.0 / height;
+    }
+    if (width > 0) {
+      theta[1] = static_cast<double>(x - mouse_pos[0]) * 180.0 / width;
+    }
+    mouse_pos[0] = x;
+    mouse_pos[1] = y;
+    angle[0] += theta[0];
+    angle[1] += theta[1];
+    glutPostRedisplay();
+  }
+}
+
 void resize(int width, int height) {
   glViewport(0, 0, width, height);
 
@@ -52,6 +99,8 @@ int main(int argc, char **argv) {
   glutCreateWindow("glut_sample");
   glutDisplayFunc(display);
   glutKeyboardFunc(keyboard);
+  glutMouseFunc(mouse);
+  glutMotionFunc(motion);
   glutReshapeFunc(resize);
   glutMainLoop();
   return 0;
